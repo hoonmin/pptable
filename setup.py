d013a9ef5b164f75ed690cc7cb2daa2665847169
Mateusz Łapsa-Malawski <mateusz@munhitsu.com>
2014-05-01 11:03:35 +0100

    version bump to release fixes from: livibetter

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pptable',
-    version='0.0.2',
+    version='0.0.3',
     url='http://github.com/munhitsu/pptable',
     license='MIT',
     author='Mateusz Lapsa-Malawski',
